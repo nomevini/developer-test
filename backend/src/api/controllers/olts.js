b5ff61ecf3382ad899bd15829654dafc8e2efd9a
Vinícius de Sousa Carvalho <sousav387@gmail.com>
2024-07-25 23:29:49 -0300

    feat: update route to capture data

--- backend/src/api/controllers/olts.js
+++ backend/src/api/controllers/olts.js
@@ -2,7 +2,7 @@ const {OltDevice}  = require('../../db/models');
 
 const getOlts = async (req, res) => {
   try {
-    const data = await OltDevice.findAll();
+    const data = await OltDevice.findAll({attributes: ['slot', 'port', 'ont_id', 'sn', 'state', 'brand']});
     res.status(200).json(data)
   } catch (error) {
     res.status(500).json({message: 'Erro interno do servidor'})
