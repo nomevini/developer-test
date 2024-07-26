84599ab827c2536af411e9bae6dd218adf3914c6
Vinícius de Sousa Carvalho <sousav387@gmail.com>
2024-07-25 23:03:44 -0300

    feat: create middleware for verify files

---
+++ backend/src/api/middleware/verifyUpload.js
@@ -0,0 +1,44 @@
+const fs = require('fs')
+const {regexZteType1, regexZteType2, regexHuawei} = require('../../../utils/regex')
+
+const verifyUpload = (req, res, next) => {
+  try {
+    
+    if (!req.file) {
+      return res.status(400).json({message: 'Nenhum arquivo foi enviado.'});
+    }
+
+    const fileContent = fs.readFileSync(req.file.path, 'utf-8');
+    const lines = fileContent.split('\n');
+    const typeArchive = -1;
+
+    for (const line in lines) {
+      if (lines[line].match(regexZteType1)) { 
+        req.type = 1
+        req.brand = 'zte'
+        break
+      }else if (lines[line].match(regexZteType2)) {
+        req.type = 2
+        req.brand = 'zte'
+        break
+      } else if (lines[line].match(regexHuawei)) {
+        req.type = 3
+        req.brand = 'huawei'
+        break
+      }
+    }
+    
+    if(!typeArchive){
+      return res.status(400).json({message: "Arquivo enviado não condiz com o padrão esperado"})
+    }
+
+    next()
+
+  } catch (error) {
+    console.log(error)
+    res.status(400).json({message: 'Erro na abertura do arquivo'})
+  }
+}
+module.exports = {
+  verifyUpload
+}
